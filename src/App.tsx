b8eac1c123651165615ea68f3f2d02aed110ba03
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-10-23 15:51:18 +0200

    FEAT font add and typography setted

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './assets/css/app.css'
+import "./assets/css/app.css";
 
 function App() {
-
   return (
-    <h1>Todo List</h1>
-  )
+    <>
+      <h1>H1</h1>
+      <p className="label">Label</p>
+      <p className="label-medium">Label_Medium</p>
+      <p className="label-strikethrough">Label_Barrato</p>
+    </>
+  );
 }
 
-export default App
+export default App;
