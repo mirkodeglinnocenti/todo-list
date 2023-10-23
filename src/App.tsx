e9908d7c3844ce00752c031fe12b2706d6983313
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-10-23 17:11:54 +0200

    FEAT button add

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,37 @@ function App() {
               </ul>
             </div>
           </div>
+          <div className="new-item-box">
+            <button className="button-new-item label-medium shadow">
+              <div className="flex-button">
+                <div className="icon-button">
+                  <svg
+                    width="40"
+                    height="40"
+                    viewBox="0 0 40 40"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M33 20L7 20"
+                      stroke="var(--text-color)"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                    />
+                    <path
+                      d="M20 33L20 7"
+                      stroke="var(--text-color)"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                    />
+                  </svg>
+                </div>
+                <div>
+
+                <span className="label-medium">Nuova voce</span>
+                </div>
+              </div>
+            </button>
+          </div>
         </div>
       </main>
     </>
