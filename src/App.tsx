64ae88ff39ddbe9f38edc8be1377e81515d08017
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-10-24 17:45:08 +0200

    FIX improved precesion of delete and check task

--- src/App.tsx
+++ src/App.tsx
@@ -10,49 +10,46 @@ function App() {
   useEffect(() => {
     const savedTasks = localStorage.getItem("tasks");
 
-    if(savedTasks){
-      setTasks(JSON.parse(savedTasks))
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
     }
-
-  },[])
+  }, []);
 
   // Order tasks
-  const undoneTasks = tasks.filter(task => task.done === false);
-  const doneTasks = tasks.filter(task => task.done === true);
+  const undoneTasks = tasks.filter((task) => task.done === false);
+  const doneTasks = tasks.filter((task) => task.done === true);
 
   // Add Task
   const addTask = (newTask: ITask) => {
-    const newTasks = [...tasks, newTask]
+    const newTasks = [...tasks, newTask];
     setTasks(newTasks);
 
-    localStorage.setItem("tasks", JSON.stringify(newTasks))
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
   };
 
   // SetDone Task
   function setDone(task: ITask) {
-
     const updatedTasks = tasks.map((el) => {
       if (el === task) {
         return { ...el, done: !el.done };
       }
       return el;
     });
-  
+
     setTasks(updatedTasks);
 
-    localStorage.setItem("tasks", JSON.stringify(updatedTasks))
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
   }
 
   // Delete Task
   const deleteTask = (index: number) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
 
-    const newTasks = [...tasks]
-    newTasks.splice(index, 1)
+    setTasks(newTasks);
 
-    setTasks(newTasks)
-    
-    localStorage.setItem("tasks", JSON.stringify(newTasks))
-  }
+    localStorage.setItem("tasks", JSON.stringify(newTasks));
+  };
 
   return (
     <>
@@ -66,26 +63,60 @@ function App() {
                   {undoneTasks.map((task, index) => {
                     return (
                       <li className="list-item" key={index}>
-                        <span onClick={()=>deleteTask(index)}>
-                        <img className="trash-icon" src="./assets/img/Bin.svg" alt="trash icon" />
+                        <img
+                          onClick={() => deleteTask(index)}
+                          className="trash-icon"
+                          src="./assets/img/Bin.svg"
+                          alt="trash icon"
+                        />
+                        <img
+                          onClick={() => setDone(task)}
+                          className="checkbox-icon"
+                          src={
+                            task.done
+                              ? "./assets/img/Checkbox_On.svg"
+                              : "./assets/img/Checkbox_Off.svg"
+                          }
+                          alt="trash icon"
+                        />
+                        <span
+                          className={
+                            task.done ? "label-strikethrough" : "label"
+                          }
+                        >
+                          {task.name}
                         </span>
-                        <span onClick={()=>setDone(task)}>
-                        <img className="checkbox-icon" src={task.done ? "./assets/img/Checkbox_On.svg" : "./assets/img/Checkbox_Off.svg"} alt="trash icon" />
-                        </span>
-                        <span className={task.done ? "label-strikethrough" : "label"}>{task.name}</span>
                       </li>
                     );
                   })}
                   {doneTasks.map((task, index) => {
                     return (
                       <li className="list-item" key={index}>
-                        <span onClick={()=>deleteTask(index)}>
-                        <img className="trash-icon" src="./assets/img/Bin.svg" alt="trash icon" />
+                        <span onClick={() => deleteTask(index)}>
+                          <img
+                            className="trash-icon"
+                            src="./assets/img/Bin.svg"
+                            alt="trash icon"
+                          />
                         </span>
-                        <span onClick={()=>setDone(task)}>
-                        <img className="checkbox-icon" src={task.done ? "./assets/img/Checkbox_On.svg" : "./assets/img/Checkbox_Off.svg"} alt="trash icon" />
+                        <span onClick={() => setDone(task)}>
+                          <img
+                            className="checkbox-icon"
+                            src={
+                              task.done
+                                ? "./assets/img/Checkbox_On.svg"
+                                : "./assets/img/Checkbox_Off.svg"
+                            }
+                            alt="trash icon"
+                          />
                         </span>
-                        <span className={task.done ? "label-strikethrough" : "label"}>{task.name}</span>
+                        <span
+                          className={
+                            task.done ? "label-strikethrough" : "label"
+                          }
+                        >
+                          {task.name}
+                        </span>
                       </li>
                     );
                   })}
